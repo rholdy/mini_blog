64bd18f1e2d68e29e8fd37b2c97c4e953648658c
Alex Howington <alex.howington@gmail.com>
2013-09-30 17:10:49 -0600

    Make index page suck less. Set Time Zone to Mountain.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.where("publish_at <=  ?", Time.now).order("publish_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
