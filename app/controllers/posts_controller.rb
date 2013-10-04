a57ff55296ab1dfefee09f63fc2f598633acbb05
DevPoint 3 <devpoint3@DevPoints-iMac-6.local>
2013-10-04 15:40:22 -0600

    Fix comments so that they actually work now.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,8 +14,8 @@ class PostsController < ApplicationController
   # GET /posts/1.json
   def show
     @post = Post.find(params[:id])
-    #@comments = @post.comments.new 
-    @comment = @post.comments.create
+    @comments = @post.comments.new
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @post }
