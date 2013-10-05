0b8c3a25e19b38d89ac1cda277968fbd51a034c3
DevPoint 3 <rholdy@gmail.com>
2013-10-04 18:31:58 -0600

    Debugged

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,10 +2,12 @@ class Author < ActiveRecord::Base
   has_many :posts
   has_many :comments, through: :posts
   attr_accessible :bio, :email, :first_name, :last_name, :twitter, :username
-  # presence
   validates :email, :first_name, :last_name, :username, presence: true
-  # unique
   validates :email, :username, uniqueness: true
-  # length
   validates :bio, length: { in: 10..500 }
+
+  def full_name 
+    (first_name + " " + last_name).titleize
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :author
-  has_many :comments
+  has_many :comments, dependent: :destroy
   attr_accessible :body, :publish_at, :title, :author_id
   validates :body, length: { in: 10..10000 }
   validates :title, presence: true
